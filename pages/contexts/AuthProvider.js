55e7672207947d900012fe85f66c249d62295a25
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2023-11-13 14:52:44 +0100

    add authorization -needs improvement

--- pages/contexts/AuthProvider.js
+++ pages/contexts/AuthProvider.js
@@ -1,10 +1,22 @@
-import React, {createContext, useContext, useState} from 'react'
+import React, { createContext, useContext, useState, useEffect } from "react"
+import {useRouter} from "next/router"
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+      router.push('/'); // Redirect to landing page if not authenticated
+    }
+  }, [router]);
 
   const saveUser = (userData) => {
     setUser(userData)
@@ -14,7 +26,8 @@ export const AuthProvider = ({children}) => {
   const clearUser = () => {
     setUser(null)
     setIsAuthenticated(false)
-    localStorage.removeItem('token') // Also clear the token from localStorage
+    localStorage.removeItem("token") // Also clear the token from localStorage
+
   }
 
   const authContextValue = {
