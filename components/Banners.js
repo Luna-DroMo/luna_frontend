fef580b27f0609e3e01546ca854d573dcb82c90b
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-01-17 10:36:35 +0100

    fixed bug regarding default role value during sign up. Added dynamic routing during signup for different roles

--- components/Banners.js
+++ components/Banners.js
@@ -9,7 +9,7 @@ export function SignupReminderBanner() {
         return (
             <div href="#" className='flex border rounded-xl bg-[#e6bf4e] mb-7 p-5 border-[#dec068] z-1'>
                 <div className="relative ml-5">
-                    <p class="align-middle">You haven't finished setting up your profile yet! <a class="underline" href="./account_setup_overview">Click here to complete!</a></p>
+                    <p className="align-middle">You haven't finished setting up your profile yet! <a className="underline" href="./account_setup_overview">Click here to complete!</a></p>
 
                 </div>
             </div>
--- components/SignupForm.js
+++ components/SignupForm.js
@@ -7,24 +7,27 @@ function SignUpForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [role, setRole] = useState('');
+  const [user_type, setUsertype] = useState('1');
   const { saveUser, setError } = useAuth()
   const router = useRouter()
 
-
+  
   const handleSignUp = async (e) => {
     e.preventDefault() // every time we submit, javascript wants to refresh. We stop this with this.
     try {
       // Axios helps us send data. Its an HTTP client. Makes things easy for us. Helps us gets the right datatype.
+      let data = {
+        user_type: user_type,
+        email: email,
+        password: password,
+        first_name: 'John',
+        last_name: 'Doe'
+      }
+
+      console.log(data)
       const register_response = await axios.post(
         'http://localhost:8000/signup',
-        {
-          user_type: role,
-          email: email,
-          password: password,
-          first_name: 'John',
-          last_name: 'Doe'
-        }
+        data
       )
     } catch (error) {
       console.error('An error occurred during sign up:', error)
@@ -42,7 +45,11 @@ function SignUpForm() {
       // Assuming the token is set in a secure, HttpOnly cookie by the server.
 
       // Redirect using Next.js Router
-      router.push('/registration_forms/benutzerdaten_input/')
+
+      // If Student
+      if (user_type === "1") { router.push('/registration_forms/benutzerdaten_input/') }
+      // If Lecturer / ADmin
+      else { router.push('/cockpit/') }
 
     } catch (error) {
       console.log("error during login", error)
@@ -55,13 +62,13 @@ function SignUpForm() {
       <legend className='mb-2'>Rolle Auswählen: </legend>
       <div className="flex w-full mb-12 justify-stretch">
 
-        <input onChange={(e) => setRole(e.target.value)} id="student" className="hidden peer/student" name="role" type="radio" defaultChecked value="1" />
+        <input onChange={(e) => setUsertype(e.target.value)} id="student" className="hidden peer/student" name="role" type="radio" defaultChecked value="1" />
         <label htmlFor="student" className='text-center bg-[#FCFAFE] border border-[#e5e7eb] w-1/3 rounded-l-xl p-3 peer-checked/student:bg-lunapurple peer-checked/student:text-white hover:bg-[#F7F3FE]'>Student</label>
 
-        <input onChange={(e) => setRole(e.target.value)} id="lecturer" className="hidden peer/lec" name="role" type="radio" value="2" />
+        <input onChange={(e) => setUsertype(e.target.value)} id="lecturer" className="hidden peer/lec" name="role" type="radio" value="2" />
         <label htmlFor="lecturer" className='text-center bg-[#FCFAFE] border-t border-b border-[#e5e7eb] w-1/3 p-3 peer-checked/lec:bg-lunapurple peer-checked/lec:text-white hover:bg-[#F7F3FE]'>Dozent</label>
 
-        <input onChange={(e) => setRole(e.target.value)} id="admin" className="hidden peer/admin" name="role" type="radio" value="3" />
+        <input onChange={(e) => setUsertype(e.target.value)} id="admin" className="hidden peer/admin" name="role" type="radio" value="3" />
         <label htmlFor="admin" className='text-center bg-[#FCFAFE] border border-[#e5e7eb] w-1/3 rounded-r-xl p-3 peer-checked/admin:bg-lunapurple peer-checked/admin:text-white hover:bg-[#F7F3FE]'>Verwaltung</label>
       </div>
 
