ad5e1be46e4062a5e648bd8ea77627b977777cc2
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-01-17 17:40:33 +0100

    added API connectivity to create modules page - half the fields are connected for now

--- pages/createModule.js
+++ pages/createModule.js
@@ -3,30 +3,76 @@ import RootLayout from '@/components/RootLayout.js';
 import { FormButton } from '@/components/Buttons';
 import { FormInput, InputRow } from '@/components/FormElements';
 import React from 'react';
+import { useState } from 'react'
+import { hasNullValue } from '@/utils/utils';
+import axios from 'axios';
 
 
 
 export default function Main() {
+    const [moduleID, setModuleID] = useState("")
+    const [moduleName, setModuleName] = useState("")
+    const [faculty, setFaculty] = useState("")
+    const [modulePassword, setModulePassword] = useState("")
+    const [modulePassword_val, setModulePassword_val] = useState("")
+    const [startDate, setStartDate] = useState("")
+    const [endDate, setEndDate] = useState("")
+    const [error_message, setErrorMessage] = useState('')
+
+
+    const data = {
+        module_id: moduleID,
+        name: moduleName,
+        faculty: faculty,
+        password: modulePassword,
+        start_date: startDate,
+        end_date: endDate
+    }
+
+    const handleModuleCreation = async (e) => {
+        e.preventDefault()
+        console.log("Writing:", data)
+
+        if (hasNullValue(data)) {
+            setErrorMessage("Missing values") 
+        } else {
+            if (modulePassword===modulePassword_val){
+                try {
+                    const response = await axios.post(
+                        `http://localhost:8000/api/createModule`,
+                        data
+                    )
+
+                    router.push("./cockpit")
+                } catch (error) {
+                    console.log("error", error)
+                    setErrorMessage(error.message)
+                }
+            } else {
+                setErrorMessage("Passwords do not match")
+            }
+        }
+    }
 
     return (
         <RootLayout>
 
             <main className="flex-row justify-between px-10 pt-10">
-                <form>
+                <form onSubmit={handleModuleCreation}>
                     <h1 className='text-2xl'>Modul Erstellen</h1>
                     <h3 className='text-text-grey'>Trage bitte alle Informationen ein, um ein Modul anzulegen.</h3>
                     <div className='h-8'></div>
-                    <InputRow type="text" maintext="Module ID" subtitle="Universitäts-interne Identifikationsnummer. Z.B. CS101" />
-                    <InputRow type="text" maintext="Module Name" subtitle="Name des Moduls. Z.B. Einführung in die Datenanalyse" />
-                    <InputRow type="text" maintext="Fakultät" subtitle="Zuständige Fakultät des Moduls" />
+                    <InputRow type="text" maintext="Module ID" subtitle="Universitäts-interne Identifikationsnummer. Z.B. CS101" onChange={(e) => {setModuleID(e.target.value)}}/>
+                    <InputRow type="text" maintext="Module Name" subtitle="Name des Moduls. Z.B. Einführung in die Datenanalyse" onChange={(e) => {setModuleName(e.target.value)}}/>
+                    <InputRow type="text" maintext="Fakultät" subtitle="Zuständige Fakultät des Moduls" onChange={(e) => {setFaculty(e.target.value)}}/>
 
                     <div className='h-8'></div>
-                    <InputRow type="password" maintext="Passwort" subtitle="Das Passwort wird bei dem Beitreten des Moduls benötigt" />
-                    <InputRow type="password" maintext="Passwort wiederholen" subtitle="" />
+                    <InputRow type="password" maintext="Passwort" subtitle="Das Passwort wird bei dem Beitreten des Moduls benötigt" onChange={(e) => {setModulePassword(e.target.value)}} />
+                    <InputRow type="password" maintext="Passwort wiederholen" subtitle="" onChange={(e) => {setModulePassword_val(e.target.value)}}/>
 
                     <div className='h-8'></div>
-                    <InputRow type="date" maintext="Startdatum" subtitle="Begin der Veranstaltung" />
-                    <InputRow type="date" maintext="Enddatum" subtitle="End der Veranstaltung" />
+                    <InputRow type="date" maintext="Startdatum" subtitle="Begin der Veranstaltung" onChange={(e) => {setStartDate(e.target.value)}} />
+                    <InputRow type="date" maintext="Enddatum" subtitle="End der Veranstaltung" onChange={(e) => {setEndDate(e.target.value)}}/>
 
                     <div className='h-8'></div>
                     <InputRow type="dropdown" options={["Vollständig", "Reduziert"]} maintext="Befragungsart" subtitle="Die Befragungsart kann nach der Erstellung nicht mehr geändert werden!" />
@@ -44,14 +90,14 @@ export default function Main() {
                     </div>
 
                     <div className='h-8'></div>
-                    <InputRow type="checkbox" options={["M", "D", "M", "D", "F", "S", "S"]} maintext="Bitte die Tage wählen, an denen die Befragungen stattfinden sollen. " subtitle="Jede einzelne Befragung ist für 3 Tagen aktiv." />
+                    <InputRow type="checkbox" options={["MO", "DI", "MI", "DO", "FR", "SA", "SO"]} maintext="Bitte die Tage wählen, an denen die Befragungen stattfinden sollen. " subtitle="Jede einzelne Befragung ist für 3 Tagen aktiv." />
                     <InputRow type="date" maintext="Ausnahme Tage [NEEDS PROPER IMPLEMENTATION]" subtitle="Falls die Befragung an manchen Tagen nicht stattfinden soll, können diese hier festgelegt werden." />
 
                     <div className='h-8'></div>
                     <InputRow type="checkbox" options={["Privat"]} maintext="Modul als Privat markieren" subtitle="Private Module werden nicht in der Suche nach Modulen erscheinen. Ein Passwort wird weiterhin benötigt." />
                     <div className="flex justify-evenly w-3/5 mt-24">
                         <FormButton text="Abbrechen" formAction="/" />
-                        <FormButton text="Modul Erstellen" highlighted="true" formAction="/" />
+                        <FormButton text="Modul Erstellen" highlighted="true" formAction="/" type='submit' />
                     </div>
                 </form>
             </main>
--- utils/utils.js
+++ utils/utils.js
@@ -2,19 +2,8 @@ import { SignupReminderBanner } from "@/components/Banners";
 
 
 
-export function checkAuthBeforeRender(isAuthenticated) {
-    // If not authenticated, the utility function would have handled the redirection
-    if (!isAuthenticated) {
-        console.log(isAuthenticated)
-        if (typeof window !== 'undefined') {
-            //window.location.replace('/');
-        }
-        return null; // You can also render a loading indicator or a login prompt
-    }
-}
-
-
 export function hasNullValue(obj) {
     return Object.values(obj).some(value => value === null);
   }
+
 
