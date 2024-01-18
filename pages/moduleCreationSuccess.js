62c80e9fe3e0882cf52441678215b985c32cbca4
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-01-18 18:56:46 +0100

    redid module creation confirmation page

--- pages/moduleCreationSuccess.js
+++ pages/moduleCreationSuccess.js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Button } from '@/components/Buttons';
+import { InputRow } from '@/components/FormElements';
 
 
 
@@ -25,22 +26,38 @@ export default function Main() {
     }, [router.isReady]);
 
 
+
     return (
         <RootLayout>
 
             <main className="flex-row justify-between px-10 pt-10">
                 <h1 className='tracking-wider text-xl text-lunapurple mb-8'>Modul Erstellt!</h1>
-                {typeof val !== 'undefined' &&
-                    <>
-                        <p>Module Name: {val["name"]}</p>
-                        <p>Module ID: {val["module_id"]}</p>
-                        <p>Fakultät: {val["faculty"]}</p>
-                        <p>Startdatum: {val["start_date"]}</p>
-                        <p>Enddatum: {val["end_date"]}</p>
-                    </>
-                }
-            <Button text="noch ein Module erstellen" highlighted="False" href="/createModule"></Button>
-            <Button text="zum Cockpit" highlighted="True" href="/cockpit"></Button>
+                <p>Module Name: Testname</p>
+                <p>Module ID: Test ID </p>
+                <p>Fakultät: Test Fac</p>
+                <input readOnly = {true}></input>
+                <p>Startdatum: 10-10-1000</p>
+                <p>Enddatum: 10-10-1</p>
+
+
+                <h3 className='text-text-grey'>Trage bitte alle Informationen ein, um ein Modul anzulegen.</h3>
+                <div className='h-8'></div>
+                <InputRow readOnly={true} type="text" maintext="Module ID" subtitle="Universitäts-interne Identifikationsnummer. Z.B. CS101"  />
+                <InputRow readOnly={true} type="text" maintext="Module Name" subtitle="Name des Moduls. Z.B. Einführung in die Datenanalyse"  />
+                <InputRow readOnly={true} type="text" maintext="Fakultät" subtitle="Zuständige Fakultät des Moduls"  />
+
+                <div className='h-8'></div>
+                <InputRow readOnly={true} type="password" maintext="Passwort" subtitle="Das Passwort wird bei dem Beitreten des Moduls benötigt"/>
+                <InputRow readOnly={true} type="password" maintext="Passwort wiederholen" subtitle=""  />
+
+                <div className='h-8'></div>
+                <InputRow readOnly={true} type="date" maintext="Startdatum" subtitle="Begin der Veranstaltung" />
+                <InputRow readOnly={true} type="date" maintext="Enddatum" subtitle="End der Veranstaltung" />
+
+                <div className="flex justify-evenly w-3/5 mt-24">
+                    <button onClick={(e) => { router.reload() }} className="border border-lunapurple rounded-full w-44 h-10 px-4 text-lunapurple text-base leading-4 hover:border-lunagreen hover:bg-lunagreen">Noch ein Modul erstellen</button>
+                    <Button text="zum Cockpit" highlighted="True" href="/cockpit"></Button>
+                </div>
             </main>
         </RootLayout>
     )
