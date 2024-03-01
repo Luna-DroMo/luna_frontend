679e5b1dfd7d161a12b1ece2cb64e0ee4127a8ad
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-03-01 19:11:49 +0300

    add new sidebar for process tracking / there is bug in dynamic percentage counter

--- components/RootLayout.js
+++ components/RootLayout.js
@@ -1,20 +1,26 @@
 import Sidebar from "./Sidebar"
 
-export default function RootLayout({ show_main_links = true, show_billboard = true, children }) {
+export default function RootLayout({
+  show_main_links = true,
+  show_billboard = true,
+  show_progress_bar = false,
+  percentage = 0,
+  children
+}) {
   // Probs doesnt need to be here. Just for testing. Unsure how to do this properly at this point.
 
   // Check if logged in
 
-    return (
+  return (
+    <>
+      <Sidebar
+        show_main_links={show_main_links}
+        show_billboard={show_billboard}
+        show_progress_bar={show_progress_bar}
+        percentage={percentage}
+      />
 
-      <>
-        <Sidebar show_main_links={show_main_links} show_billboard={show_billboard} />
-
-        <div className="mainbody">
-          {children}</div>
-
-      </>
-
-    )
+      <div className='mainbody'>{children}</div>
+    </>
+  )
 }
-
