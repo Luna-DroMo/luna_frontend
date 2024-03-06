2abea0f457639ccacf590e0d33f948d71d3c9116
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-03-06 15:52:59 +0100

    added plot and placeholder functionality to analysis page

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,32 @@
 import { SignupReminderBanner } from "@/components/Banners";
 
+export function Add(list1, list2){
+
+  
+  // Check if both lists have the same length
+  if (list1.length !== list2.length) {
+    return <div>Lists must have the same length</div>;
+  }
+  
+  // Perform addition
+  const result = list1.map((num, index) => num + list2[index]);
+  
+  return(result);
+};
+
+export function Subtract(list1, list2){
+
+  
+  // Check if both lists have the same length
+  if (list1.length !== list2.length) {
+    return <div>Lists must have the same length</div>;
+  }
+  
+  // Perform addition
+  const result = list1.map((num, index) => num - list2[index]);
+  
+  return (result);
+};
 
 
 
