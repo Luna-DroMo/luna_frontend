1087cfceeaca1240960fb0acb299c73abe88911b
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-03-24 13:13:44 +0100

    removed shadow on person tag

--- components/PersonTag.js
+++ components/PersonTag.js
@@ -2,7 +2,7 @@ export default function Person({ img, name }) {
     return (
         <div className='flex'>
 
-            <div className='border rounded-md border-[#dddddd] inline-block flex items-center px-0 hover:bg-white hover:shadow-sm hover:shadow-lunapurple'>
+            <div className='border rounded-md border-[#dddddd] inline-block flex items-center px-0 hover:bg-white hover:shadow-sm'>
 
                 <img src={img} className='w-4 ml-1 float-left shrink fill-lunapurple' />
                 <span className={'text-base text-[#4a4a4a] px-4 shrink'}>{name}</span>
