d5c1933b02d79823c9747a63c6bc3b4cc403aa28
Dennis Perrett <dennisperrett@Denniss-MBP-3.fritz.box>
2024-03-24 10:44:01 +0100

    updated data transfer between find module and join module pages

--- pages/module_search.js
+++ pages/module_search.js
@@ -12,31 +12,31 @@ export default function Main() {
 
 
     const modules_open = [
-        { code: 'QDS-FO1', name: 'Mathematical Introduction', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Erin Ritchie" } },
-        { code: 'QDS-FO2', name: 'Advanced Statistics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Mustermann" } },
-        { code: 'QDS-FO3', name: 'Programming I', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 2, staff: { img: "user.png", name: "Dennis Perrett" } },
-        { code: 'QDS-FO4', name: 'Programming II', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 3, staff: { img: "user.png", name: "Dennis Perrett" } },
-        { code: 'QDS-FO5', name: 'Experimental and Quasi Experimental Design', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 4, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-FO6', name: 'Ethics Seminar', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 4, staff: { img: "user.png", name: "Dennis Perrett" } },
-        { code: 'QDS-FO7', name: 'Bayesian Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 3, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-FO8', name: 'Research Seminar', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-PS1', name: 'Foundations in Psychometrics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
-        { code: 'QDS-PS2', name: 'Psychometrics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
-        { code: 'QDS-PS3', name: 'Item Response Theory', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-PS4', name: 'Mathematical Models in Psychology', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-PS5', name: 'Longitudinal Data Analysis', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
-        { code: 'QDS-PS6', name: 'Multilevel Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-PS7', name: 'Structured Equation Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-PS8', name: 'Latent Variable Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-EC2', name: 'Applied Econometrics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-EC3', name: 'Advanced Times-series Analysis', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
-        { code: 'QDS-EC4', name: 'Advanced Microeconomics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-EC5', name: 'Machine Learning in Econometrics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-ML2', name: 'Machine Learning I', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
-        { code: 'QDS-ML3', name: 'Data Literacy', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
-        { code: 'QDS-ML4', name: 'Deep Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-ML5', name: 'Statistical Machine Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
-        { code: 'QDS-ML6', name: 'Probabilistic Machine Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 12345, code: 'QDS-FO1', name: 'Mathematical Introduction', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Erin Ritchie" } },
+        { id: 12344, code: 'QDS-FO2', name: 'Advanced Statistics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Mustermann" } },
+        { id: 12346, code: 'QDS-FO3', name: 'Programming I', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 2, staff: { img: "user.png", name: "Dennis Perrett" } },
+        { id: 22345, code: 'QDS-FO4', name: 'Programming II', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 3, staff: { img: "user.png", name: "Dennis Perrett" } },
+        { id: 65454, code: 'QDS-FO5', name: 'Experimental and Quasi Experimental Design', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 4, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 34324, code: 'QDS-FO6', name: 'Ethics Seminar', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 4, staff: { img: "user.png", name: "Dennis Perrett" } },
+        { id: 34235, code: 'QDS-FO7', name: 'Bayesian Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 3, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 98876, code: 'QDS-FO8', name: 'Research Seminar', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 764454, code: 'QDS-PS1', name: 'Foundations in Psychometrics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
+        { id: 456456, code: 'QDS-PS2', name: 'Psychometrics', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
+        { id: 567567, code: 'QDS-PS3', name: 'Item Response Theory', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 456452, code: 'QDS-PS4', name: 'Mathematical Models in Psychology', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 111111, code: 'QDS-PS5', name: 'Longitudinal Data Analysis', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
+        { id: 222222, code: 'QDS-PS6', name: 'Multilevel Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 333333, code: 'QDS-PS7', name: 'Structured Equation Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 444444, code: 'QDS-PS8', name: 'Latent Variable Modeling', faculty: 'Methods Center', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 788888, code: 'QDS-EC2', name: 'Applied Econometrics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 777777, code: 'QDS-EC3', name: 'Advanced Times-series Analysis', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
+        { id: 888888, code: 'QDS-EC4', name: 'Advanced Microeconomics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 999999, code: 'QDS-EC5', name: 'Machine Learning in Econometrics', faculty: 'Economics and Social Sciences', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 101010, code: 'QDS-ML2', name: 'Machine Learning I', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Augustin Kelava" } },
+        { id: 121212, code: 'QDS-ML3', name: 'Data Literacy', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
+        { id: 343434, code: 'QDS-ML4', name: 'Deep Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 565656, code: 'QDS-ML5', name: 'Statistical Machine Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Max Musterman" } },
+        { id: 878689, code: 'QDS-ML6', name: 'Probabilistic Machine Learning', faculty: 'Computer Science', semester: 'Summer Semester 2024', status: 1, staff: { img: "user.png", name: "Cagatay Ilmaz" } },
 
     ]
 
@@ -79,8 +79,9 @@ export default function Main() {
                             </tr>
                         </thead>
                         <tbody className='rounded-b-lg'>
-                            {modules.map((module, index) => (
-                                <TableRow key={index}
+                            {modules.map((module) => (
+                                <TableRow key={module.id}
+                                    id = {module.id}
                                     code={module.code}
                                     name={module.name}
                                     semester={module.semester}
@@ -98,7 +99,7 @@ export default function Main() {
 }
 
 
-function TableRow({ code, name, semester, status, faculty, staff }) {
+function TableRow({ id, code, name, semester, status, faculty, staff }) {
     return (
         <tr href="#" className='items-center h-12 odd:bg-white even:bg-[#fafafa] hover:bg-[#f0f0f0]'>
             <td className="">
@@ -112,7 +113,7 @@ function TableRow({ code, name, semester, status, faculty, staff }) {
             <td className='text-[#4a4a4a] text-base tracking-wide'>{semester} </td>
             <td > <Status status={status} /></td>
             <td> <Person img={staff.img} name={staff.name} /></td>
-            <td> <Link href={{pathname: "join_module", query: {code: code, name: name, semester: semester, faculty: faculty, staff: staff.name}}} className='' title="Modul Beitreten"> <img src="join.png" className='w-4 mx-auto' /></Link></td>
+            <td> <Link href={{pathname: "join_module", query: {id: id, code: code, name: name, semester: semester, faculty: faculty, staff: staff.name}}} className='' title="Modul Beitreten"> <img src="join.png" className='w-4 mx-auto' /></Link></td>
 
         </tr>
     )
