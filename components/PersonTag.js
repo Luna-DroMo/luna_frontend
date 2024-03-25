ab162a0e3bf53c9e81141ba0eaf9def58360be81
Dennis Perrett <perrett.dennis@gmail.com>
2024-03-25 11:07:20 +0100

    updated lecturer cockpit, included user_type check for staff_registration, changed logo (again)

--- components/PersonTag.js
+++ components/PersonTag.js
@@ -4,7 +4,7 @@ export default function Person({ img, name }) {
 
             <div className='border rounded-md border-[#dddddd] inline-block flex items-center px-0 hover:bg-white hover:shadow-sm'>
 
-                <img src={img} className='w-4 ml-1 float-left shrink fill-lunapurple' />
+                <img src={'pleaceholder_headshot.jpg'} className='w-4 h-4 ml-1 float-left shrink fill-lunapurple rounded-full border-2 border-lunapurple' />
                 <span className={'text-base text-[#4a4a4a] px-4 shrink'}>{name}</span>
 
             </div>
--- pages/cockpit_pages/cockpit_lecturer.js
+++ pages/cockpit_pages/cockpit_lecturer.js
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import Table from '@/components/Table.js'
-import AlertSection from '@/components/Alerts.js'
+import { LecturerModuleTable } from '@/components/Table.js'
 import RootLayout from '@/components/RootLayout.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +9,7 @@ import { useRouter } from 'next/router';
 import { SignupReminderBanner } from '@/components/Banners.js'
 import axios from 'axios'
 import { useEffect } from 'react'
+import Link from 'next/link.js'
 
 export default function cockpit_lecturer() {
 
@@ -19,69 +19,92 @@ export default function cockpit_lecturer() {
   const { user, isAuthenticated, saveUser, clearUser } = useAuth();
   // If not authenticated, the utility function would have handled the redirection
 
-  // Page logic for an authenticated user
 
+  const [modules, setModules] = useState([])
+
+
+  useEffect(() => {
+    const getModules = async (e) => {
+      //e.preventDefault()
+
+      try {
+        //const response = await axios.get(
+        //  `http://52.5.114.46/api/student/${user.id}/modules`
+        //)
+
+        setModules(response.data)
+      } catch (error) {
+        console.log("error during login", error)
+      }
+
+    }
+
+    getModules()
+    console.log(modules)
+  }, [])
+
+  const avg_response_rate = 0.8522
 
   return (
     <RootLayout show_billboard={true}>
       <main className='flex-row justify-between px-10 pt-10'>
-      <div className='flex items-center'>
-            <img src='alien.png' className='ml-2 w-10 mr-4' />
-            <h1 className='tracking-wider text-xl'>
-              Hey {user.first_name}, willkommen bei Luna!
-            </h1>
-          </div>
-          <div className='mt-8 flex space-x-2 mb-12'>
-            <div className='flex-1 items-center rounded-xl p-2 bg-lightpurple pl-6'>
-              <div className='flex items-center'>
-                <div className='metric my-1 bg-lunapurple w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
-                  4
-                </div>
-                <div className='metric-text tracking-wider '>
-                  <p>Modulen beigetreten</p>
-                </div>
+        <div className='flex items-center'>
+          <img src='alien.png' className='ml-2 w-10 mr-4 rounded-full bg-[#cccccc] border-2 border-lunapurple' />
+          <h1 className='tracking-wider text-xl'>
+            Hey {user.first_name}, willkommen bei Luna!
+          </h1>
+        </div>
+        <div className='mt-8 flex space-x-2 mb-12'>
+          <div className='flex-1 items-center rounded-xl p-2 bg-lightpurple pl-6'>
+            <div className='flex items-center'>
+              <div className='metric my-1 bg-lunapurple w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
+                {modules.length}
               </div>
-            </div>
-            <div className='flex-1 items-center rounded-xl p-2 bg-lightgreen pl-6'>
-              <div className='flex items-center'>
-                <div className='metric my-1 bg-green w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
-                  15%
-                </div>
-                <div className='metric-text tracking-wider '>
-                  <p>Rückmeldequote</p>
-                </div>
-              </div>
-            </div>
-            <div
-              className='flex-1 relative items-center rounded-xl p-2 bg-zoom bg-center pl-6 bg-lunapurple'
-              style={{ backgroundImage: 'url("solarsystem.png")' }}
-            >
-              <div className='absolute inset-0 bg-white opacity-10'></div>
-              <div className='flex items-center'>
-                <div className='metric my-1 bg-orange z-10 w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
-                  84
-                </div>
-                <div className='tracking-wider metric-text z-10 '>
-                  <p>Highlight-text</p>
-                </div>
+              <div className='metric-text tracking-wider '>
+                <p>Modulen beigetreten</p>
               </div>
             </div>
           </div>
-          <div className='flex w-full items-end'>
-            <h5 className='tracking-wider items-center flex-grow'>
-              Meine Module
-            </h5>
-            <a href='#' className='flex-none text-lunapurple text-base pb-0.5'>
-              Module verwalten
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                className='flex group-hover:text-lunapurple inline-block w-2 mx-1.5'
-              />
-            </a>
+          <div className='flex-1 items-center rounded-xl p-2 bg-lightgreen pl-6'>
+            <div className='flex items-center'>
+              <div className='metric my-1 bg-green w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
+                {Math.round(avg_response_rate*100)}%
+              </div>
+              <div className='metric-text tracking-wider '>
+                <p>Rückmeldequote</p>
+              </div>
+            </div>
           </div>
+          <div
+            className='flex-1 relative items-center rounded-xl p-2 bg-zoom bg-center pl-6 bg-lunapurple'
+            style={{ backgroundImage: 'url("solarsystem.png")' }}
+          >
+            <div className='absolute inset-0 bg-white opacity-10'></div>
+            <div className='flex items-center'>
+              <div className='metric my-1 bg-orange z-10 w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white mr-4'>
+                84
+              </div>
+              <div className='tracking-wider metric-text z-10 '>
+                <p>Highlight-text</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex w-full items-end'>
+          <h5 className='tracking-wider items-center flex-grow'>
+            Meine Module
+          </h5>
+          <a href='#' className='flex-none text-lunapurple text-base pb-0.5'>
+            Module verwalten
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className='flex group-hover:text-lunapurple inline-block w-2 mx-1.5'
+            />
+          </a>
+        </div>
 
-        <a
-          onClick={(e) => router.push("./createModule/")}
+        <Link
+          href="./createModule"
           className='cursor-pointer flex border border-lightpurple rounded-full my-5 s-50 py-3 pl-5 hover:border-lunapurple z-1'
         >
           <div className='inline-block w-3 h-3 relative items-center z-10 my-1'>
@@ -92,8 +115,8 @@ export default function cockpit_lecturer() {
           <h4 className='ml-5 text-lunapurple tracking-wider text-base z-10'>
             Modul erstellen
           </h4>
-        </a>
-        <Table />
+        </Link>
+        <LecturerModuleTable  />
       </main>
     </RootLayout>
   )
