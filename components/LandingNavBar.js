8c5251b000d0a96d11323e247d08a41fb1c65328
Dennis Perrett <perrett.dennis@gmail.com>
2024-03-25 17:24:10 +0100

    updated landing page

--- components/LandingNavBar.js
+++ components/LandingNavBar.js
@@ -1,26 +1,26 @@
-export default function Navbar({theme}) {
+export default function Navbar({ theme }) {
 
-    let logo_source = "LogoWhite2.svg"
-    let text_color="white";
-    if (theme === "dark"){
-        logo_source = "LogoPurple2.svg"
+    let logo_source = "crap_logo.png"
+    let text_color = "white";
+    if (theme === "dark") {
+        logo_source = "crap_logo.png"
         text_color = "lunapurple"
     }
 
 
     return (
-        <>
-        <div className="w-screen pt-20 flex-none">
-            <div id="nav-links" className={`text-${ text_color } text-base`}>
-                <ul className='space-x-6 flex flex-row justify-end mr-52'>
-                    <li className='hover:underline'><a href="#">Über Uns</a></li>
-                    <li className='hover:underline'><a href="#">FAQs</a></li>
-                    <li className='hover:underline'><a href="#">Kontakt</a></li>
-                    <li className='hover:underline'><a href="#">Datenschutz</a></li>
-                    <li className='pl-5'><a href="#"><img src={logo_source} className='w-20 inline-block' alt="logo" /></a></li>
-                </ul>
-            </div>
+
+
+        <div id="nav-links" className={`text-${text_color} text-base w-full`}>
+            <ul className='space-x-6 flex flex-row justify-end items-center'>
+                <li className='hover:underline'><a href="#">Über Uns</a></li>
+                <li className='hover:underline'><a href="#">FAQs</a></li>
+                <li className='hover:underline'><a href="#">Kontakt</a></li>
+                <li className='hover:underline'><a href="#">Datenschutz</a></li>
+                <li className='pl-5'><a href="#"><img src={logo_source} className='w-6 h-6 inline-block' alt="logo" /></a></li>
+            </ul>
         </div>
-        </>
+
+
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,41 @@ import Navbar from '@/components/LandingNavBar'
 export default function Home() {
 
     return (
-        <div className='w-screen h-screen bg-lunapurple'>
-
-            <Navbar theme="light"/>
-            <div className='flex items-center'>
-                <div className='flex-1 flex items-center'>
-                    <div id="left-container" className='mx-auto pt-48'>
-                        <h1 className="text-center text-3xl text-white tracking-wider">Willkommen bei Luna!</h1>
-                        <img src="rocket.png" className='w-48 mx-auto my-8' alt="Rocket" />
-                        <p className="text-center text-white tracking-wider">Lass uns gemeinsam nach den Sternen greifen!</p>
+        <div className='w-screen min-h-screen bg-pagebg'>
+            <div className='px-48 py-12'>
+                <Navbar theme="dark" />
+                <div className='flex space-x-12 items-center'>
+                    <div className='flex-1 items-centerspace-x-12'>
+                        <div id="left-container" className='mx-auto pt-32'>
+                            <h1 className=" text-4xl text-lightgrey tracking-wider">Get real time <span className='text-lunapurple'>Feedback</span>, </h1>
+                            <h1 className=" text-4xl text-lightgrey tracking-wider"><span className='text-lunapurple'>increase</span> student retention</h1>
+                            <h1 className=" text-4xl text-lightgrey tracking-wider">and reach new heights with <span className='text-lunapurple'>Luna!</span> </h1>
+                            <p className="text-lightgrey tracking-wider">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan, purus in sodales tincidunt, lorem eros fermentum nisl, id tempor nulla leo ac ex. Nam vitae elit eu ante condimentum tincidunt vitae eu mauris.</p>
+                            <LoginForm />
+                        </div>
                     </div>
+                    <div className='flex-1 flex items-center h-full'>
+                        <div id="right-container" className='mx-auto w-full h-full flex flex-col pt-12 items-center'>
+
+                            <img src="landingimg.jpg" className='h-96 w-96 rounded-full'/>
+                        </div>
+                    </div>
+
                 </div>
-                <div className='flex-1 flex items-center h-full'>
-                    <div id="right-container" className='mx-auto w-full h-full flex flex-col pt-48 items-center'>
 
-                        <LoginForm />
-                    </div>
+            </div>
+            <div className='h-32 bg-lunapurple w-screen'>
+
+            </div>
+
+            <div className='h-screen bg-pagebg w-screen px-48 py-12'>
+
+                <h1 className='text-xl'>Get Advanced Analytics</h1>
+                <div className='bg-white rounded-xl'>
+                <img src="analysis1.png" className='m-auto w-4/6' />
                 </div>
             </div>
+
         </div >
 
 
