dc6c2b6dc798633848b76d6c6e84b518259c6635
Dennis Perrett <perrett.dennis@gmail.com>
2024-05-22 10:19:57 +0200

    removed excess from landing page

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
                         <div id="left-container" className='mx-auto pt-32'>
                             <h1 className=" text-4xl text-lightgrey tracking-wider">Get real time <span className='text-lunapurple'>feedback</span>, </h1>
                             <h1 className=" text-4xl text-lightgrey tracking-wider"><span className='text-lunapurple'>prevent</span>  and <span className='text-lunapurple'>predict</span> student dropout and reach new heights with <span className='text-lunapurple'>Luna!</span> </h1>
-                            <p className="text-lightgrey tracking-wider">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan, purus in sodales tincidunt, lorem eros fermentum nisl, id tempor nulla leo ac ex. Nam vitae elit eu ante condimentum tincidunt vitae eu mauris.</p>
+                            
                             {/* Your content */}
                             {!isAuthenticated && <LoginForm />}
                             {isAuthenticated && (
@@ -68,29 +68,7 @@ export default function Home() {
 
             </div>
 
-            <div className='h-screen bg-pagebg w-screen px-48 py-12'>
-
-
-                <div className='px-12 py-12'>
-
-                    <div className='flex space-x-12'>
-                        <div className='flex-1 items-center '>
-                            <h1 className='text-xl'>Get Advanced Analytics</h1>
-                            <p className='text-justify mt-4'>Maecenas sodales elementum dui at consectetur. Nulla tempus tincidunt est, eu auctor nulla cursus ac. Sed sed scelerisque massa. Nam viverra mauris in imperdiet condimentum.</p>
-                            <p className='text-justify mt-4'> Proin non nunc fermentum nisi cursus hendrerit. Nullam euismod purus in vehicula pharetra. Nunc semper commodo fermentum. Morbi ornare diam metus, in ornare metus blandit vitae. Maecenas in est sit amet lacus laoreet ullamcorper vel vitae leo. Curabitur ac sapien et lorem porta eleifend. Ut libero lacus, semper ultricies sem blandit, efficitur ornare lectus. Sed bibendum, lorem at mattis faucibus, lacus velit accumsan felis, pharetra dignissim risus metus vitae felis. Vestibulum nec magna ex.</p>
-                        </div>
-                        <div className='flex-1 flex items-center h-full'>
-                            <div className='bg-white rounded-xl p-2'>
-                                <img src="analysis1.png" className='m-auto w-full' />
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-
-            </div>
+         
 
         </div >
 
