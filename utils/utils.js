048328c8fa40388be6e93d2a8778279e2ba42be2
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-05-23 13:37:22 +0200

    we are getting there

--- utils/utils.js
+++ utils/utils.js
@@ -1,37 +1,33 @@
-import { SignupReminderBanner } from "@/components/Banners";
+import {SignupReminderBanner} from "@/components/Banners"
 
-export function Add(list1, list2){
+export function Add(list1, list2) {
+    // Check if both lists have the same length
+    if (list1.length !== list2.length) {
+        return <div>Lists must have the same length</div>
+    }
 
-  
-  // Check if both lists have the same length
-  if (list1.length !== list2.length) {
-    return <div>Lists must have the same length</div>;
-  }
-  
-  // Perform addition
-  const result = list1.map((num, index) => num + list2[index]);
-  
-  return(result);
-};
+    // Perform addition
+    const result = list1.map((num, index) => num + list2[index])
 
-export function Subtract(list1, list2){
+    return result
+}
 
-  
-  // Check if both lists have the same length
-  if (list1.length !== list2.length) {
-    return <div>Lists must have the same length</div>;
-  }
-  
-  // Perform addition
-  const result = list1.map((num, index) => num - list2[index]);
-  
-  return (result);
-};
+export function Subtract(list1, list2) {
+    // Check if both lists have the same length
+    if (list1.length !== list2.length) {
+        return <div>Lists must have the same length</div>
+    }
 
+    // Perform addition
+    const result = list1.map((num, index) => num - list2[index])
 
+    return result
+}
 
 export function hasNullValue(obj) {
-    return Object.values(obj).some(value => value === null);
-  }
+    return Object.values(obj).some((value) => value === null)
+}
 
-
+export function basicDateFormat(dateTimeString) {
+    return dateTimeString.split("T")[0]
+}
