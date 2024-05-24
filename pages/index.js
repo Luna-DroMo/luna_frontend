6d27c008939dd9b458c814bad4c63a11c611a256
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-05-24 10:46:19 +0200

    doing design ;) - I just changed logos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import LoginForm from '@/components/LoginForm'
-import Navbar from '@/components/LandingNavBar'
-import { useAuth } from '@/components/AuthProvider';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import Image from "next/image"
+import LoginForm from "@/components/LoginForm"
+import Navbar from "@/components/LandingNavBar"
+import {useAuth} from "@/components/AuthProvider"
+import {useState} from "react"
+import {useEffect} from "react"
+import {useRouter} from "next/navigation"
 
 /*
 
@@ -12,29 +12,34 @@ import { useRouter } from 'next/navigation';
          <Navbar/>
 */
 
-
 export default function Home() {
-
-    const { user, isAuthenticated, saveUser, clearUser } = useAuth();
-    const router = useRouter();
+    const {user, isAuthenticated, saveUser, clearUser} = useAuth()
+    const router = useRouter()
 
     return (
         <div className='w-screen min-h-screen bg-pagebg'>
             <div className='px-48 py-12'>
-                <Navbar theme="dark" />
+                <Navbar theme='dark' />
                 <div className='flex space-x-12 items-center'>
                     <div className='flex-1 items-center space-x-12'>
-                        <div id="left-container" className='mx-auto pt-32'>
-                            <h1 className=" text-4xl text-lightgrey tracking-wider">Get real time <span className='text-lunapurple'>feedback</span>, </h1>
-                            <h1 className=" text-4xl text-lightgrey tracking-wider"><span className='text-lunapurple'>prevent</span>  and <span className='text-lunapurple'>predict</span> student dropout and reach new heights with <span className='text-lunapurple'>Luna!</span> </h1>
-                            
+                        <div id='left-container' className='mx-auto pt-32'>
+                            <h1 className=' text-4xl text-lightgrey tracking-wider'>
+                                Get real time <span className='text-lunapurple'>feedback</span>,{" "}
+                            </h1>
+                            <h1 className=' text-4xl text-lightgrey tracking-wider'>
+                                <span className='text-lunapurple'>prevent</span> and{" "}
+                                <span className='text-lunapurple'>predict</span> student dropout and
+                                reach new heights with{" "}
+                                <span className='text-lunapurple'>Luna!</span>{" "}
+                            </h1>
+
                             {/* Your content */}
                             {!isAuthenticated && <LoginForm />}
                             {isAuthenticated && (
                                 <div className='mt-4 w-full rounded'>
                                     <div className='flex w-full space-x-4'>
                                         <a
-                                            href="cockpit"
+                                            href='cockpit'
                                             className='mt-6 w-52 h-12 text-black bg-lunapurple px-12 pb-2 pt-3 rounded-lg text-white hover:bg-lunagreen text-center align-middle'
                                         >
                                             Zum Cockpit
@@ -42,38 +47,30 @@ export default function Home() {
                                     </div>
                                 </div>
                             )}
-
                         </div>
                     </div>
                     <div className='flex-1 flex items-center h-full'>
-                        <div id="right-container" className='mx-auto w-full h-full flex flex-col pt-12 items-center'>
-
-                            <img src="landingimg.jpg" className='h-96 w-96 rounded-full object-cover' />
+                        <div
+                            id='right-container'
+                            className='mx-auto w-full h-full flex flex-col pt-12 items-center justify-between'
+                        >
+                            <img
+                                src='landingimg.jpg'
+                                className='h-96 w-full rounded-full object-cover'
+                            />
                         </div>
                     </div>
-
                 </div>
-
             </div>
-            
+
             <div className='h-32 bg-pagebg w-screen flex items-center text-center justify-items-center px-48'>
-            
                 <div className='flex-1'>
-                    <img src="uni_tuebingen_logo.png" className='m-auto w-40 h-18' />
+                    <img src='uni_tuebingen_logo.png' className='m-auto w-40 h-18' />
                 </div>
                 <div className='flex-1'>
-                    <img src="ministry.png" className='m-auto w-40 h-18' />
+                    <img src='ministry.png' className='m-auto w-40 h-18' />
                 </div>
-
-
             </div>
-
-         
-
-        </div >
-
-
-
-
+        </div>
     )
 }
