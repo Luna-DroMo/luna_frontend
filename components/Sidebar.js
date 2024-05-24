4125d2b0bf383b3e6ac424e65c93dc27defa41cd
Dennis Perrett <perrett.dennis@gmail.com>
2024-05-24 15:12:46 +0200

    resized logo

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -36,7 +36,7 @@ export default function Sidebar({show_main_links, show_billboard, show_progress_
             >
                 <div className='flex flex-col justify-between h-full p-6'>
                     <div className='flex items-center p-5 mb-2'>
-                        <img src='/Logo.png' className='inline-block w-16 mr-4' />
+                        <img src='/Logo.png' className='inline-block w-10 mr-4' />
                         <p className='text-3xl'>LUNA</p>
                     </div>
                     <SidebarContent
