5b948f1007f0e83e3cb1b5ed12a4d17f0136b76d
Dennis Perrett <perrett.dennis@gmail.com>
2024-05-29 10:49:29 +0200

    generalised motivation form. Added information to the forms. Removed English. Made items more readable

--- pages/registration_forms/AIST_input.js
+++ pages/registration_forms/AIST_input.js
@@ -2,73 +2,73 @@ import Image from "next/image"
 import InputLayout from "@/components/InputLayout.js"
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import { faChevronRight} from '@fortawesome/free-solid-svg-icons';
-import {FormButton} from "@/components/Buttons"
-import {FormInput, InputRow} from "@/components/FormElements"
-import {Progressbar} from "@/components/InputProgressTracker"
+import { FormButton } from "@/components/Buttons"
+import { FormInput, InputRow } from "@/components/FormElements"
+import { Progressbar } from "@/components/InputProgressTracker"
 import React from "react"
-import {useAuth} from "../../components/AuthProvider"
-import {useState} from "react"
+import { useAuth } from "../../components/AuthProvider"
+import { useState } from "react"
 import axios from "axios"
-import Router, {useRouter} from "next/router"
-import {ErrorBanner} from "@/components/Errors"
-import {hasNullValue} from "@/utils/utils"
-import {useEffect} from "react"
-import {url} from "@/utils/data"
+import Router, { useRouter } from "next/router"
+import { ErrorBanner } from "@/components/Errors"
+import { hasNullValue } from "@/utils/utils"
+import { useEffect } from "react"
+import { url } from "@/utils/data"
 
 const AIST_questions = [
-    {id: 1, question: "Mit Maschinen oder technischen Geräten arbeiten.", subtitle: ""},
-    {id: 2, question: "In einem Versuchslabor Experimente durchführen.", subtitle: ""},
+    { id: 1, question: "Mit Maschinen oder technischen Geräten arbeiten", subtitle: "" },
+    { id: 2, question: "In einem Versuchslabor Experimente durchführen", subtitle: "" },
     {
         id: 3,
-        question: "Etwas nach künstlerischen Gesichtspunkten gestalten.",
+        question: "Etwas nach künstlerischen Gesichtspunkten gestalten",
         subtitle: ""
     },
-    {id: 4, question: "Andere Personen betreuen oder pflegen.", subtitle: ""},
-    {id: 5, question: "Eine Diskussion leiten", subtitle: ""},
-    {id: 6, question: "Eine Buchhaltung führen.", subtitle: ""},
-    {id: 7, question: "In einen Computer Teile einbauen.", subtitle: ""},
-    {id: 8, question: "Wissenschaftliche Artikel lesen.", subtitle: ""},
+    { id: 4, question: "Andere Personen betreuen oder pflegen", subtitle: "" },
+    { id: 5, question: "Eine Diskussion leiten", subtitle: "" },
+    { id: 6, question: "Eine Buchhaltung führen", subtitle: "" },
+    { id: 7, question: "In einen Computer Teile einbauen", subtitle: "" },
+    { id: 8, question: "Wissenschaftliche Artikel lesen", subtitle: "" },
     {
         id: 9,
-        question: "Dinge tun, bei denen es auf Kreativität und Phantasie ankommt.",
+        question: "Dinge tun, bei denen es auf Kreativität und Phantasie ankommt",
         subtitle: ""
     },
-    {id: 10, question: "Sich die Probleme anderer Menschen anhören.", subtitle: ""},
-    {id: 11, question: "Für eine Sache in der Öffentlichkeit auftreten.", subtitle: ""},
-    {id: 12, question: "Geschäftsbriefe schreiben.", subtitle: ""},
-    {id: 13, question: "Konstruktionspläne zeichnen.", subtitle: ""},
-    {id: 14, question: "Sich mit unerforschten Dingen beschäftigen.", subtitle: ""},
-    {id: 15, question: "Dinge schön gestalten (formen, verzieren, schmücken).", subtitle: ""},
-    {id: 16, question: "Sich für die Anliegen anderer einsetzen.", subtitle: ""},
-    {id: 17, question: "Andere von etwas überzeugen oder etwas veranlassen.", subtitle: ""},
-    {id: 18, question: "Angebote einholen und vergleichen.", subtitle: ""},
-    {id: 19, question: "Elektrische Geräte oder Anlagen bauen.", subtitle: ""},
+    { id: 10, question: "Sich die Probleme anderer Menschen anhören", subtitle: "" },
+    { id: 11, question: "Für eine Sache in der Öffentlichkeit auftreten", subtitle: "" },
+    { id: 12, question: "Geschäftsbriefe schreiben", subtitle: "" },
+    { id: 13, question: "Konstruktionspläne zeichnen", subtitle: "" },
+    { id: 14, question: "Sich mit unerforschten Dingen beschäftigen", subtitle: "" },
+    { id: 15, question: "Dinge schön gestalten (formen, verzieren, schmücken)", subtitle: "" },
+    { id: 16, question: "Sich für die Anliegen anderer einsetzen", subtitle: "" },
+    { id: 17, question: "Andere von etwas überzeugen oder etwas veranlassen", subtitle: "" },
+    { id: 18, question: "Angebote einholen und vergleichen", subtitle: "" },
+    { id: 19, question: "Elektrische Geräte oder Anlagen bauen", subtitle: "" },
     {
         id: 20,
-        question: "Chemische, physikalische oder biologische Versuche durchführen.",
+        question: "Chemische, physikalische oder biologische Versuche durchführen",
         subtitle: ""
     },
-    {id: 21, question: "Bilder malen, zeichnen.", subtitle: ""},
-    {id: 22, question: "Hilfsbedürftige Kinder oder Erwachsene betreuen.", subtitle: ""},
-    {id: 23, question: "Mit anderen Menschen verhandeln.", subtitle: ""},
-    {id: 24, question: "Über etwas Aufzeichnungen oder Listen führen.", subtitle: ""},
-    {id: 25, question: "Etwas nach einem Plan oder einer Skizze anfertigen.", subtitle: ""},
-    {id: 26, question: "Die Ursache eines Problems erforschen.", subtitle: ""},
-    {id: 27, question: "Etwas mit sprachlichen Mitteln künstlerisch gestalten.", subtitle: ""},
-    {id: 28, question: "Sich in die Situation anderer Menschen hineindenken.", subtitle: ""},
-    {id: 29, question: "Das Amt des Sprechers in einer Gruppe übernehmen.", subtitle: ""},
-    {id: 30, question: "Eine Abrechnung kontrollieren.", subtitle: ""}
+    { id: 21, question: "Bilder malen, zeichnen", subtitle: "" },
+    { id: 22, question: "Hilfsbedürftige Kinder oder Erwachsene betreuen", subtitle: "" },
+    { id: 23, question: "Mit anderen Menschen verhandeln", subtitle: "" },
+    { id: 24, question: "Über etwas Aufzeichnungen oder Listen führen", subtitle: "" },
+    { id: 25, question: "Etwas nach einem Plan oder einer Skizze anfertigen", subtitle: "" },
+    { id: 26, question: "Die Ursache eines Problems erforschen", subtitle: "" },
+    { id: 27, question: "Etwas mit sprachlichen Mitteln künstlerisch gestalten", subtitle: "" },
+    { id: 28, question: "Sich in die Situation anderer Menschen hineindenken", subtitle: "" },
+    { id: 29, question: "Das Amt des Sprechers in einer Gruppe übernehmen", subtitle: "" },
+    { id: 30, question: "Eine Abrechnung kontrollieren", subtitle: "" }
 ]
 
 const breadcrump = [
-    {name: "Allgemeiner Interessen Struktur-Test", status: 1, item: 1},
-    {name: "Positive - Negative", status: 0, item: 2},
-    {name: "Motivation", status: 0, item: 3}
+    { name: "Allgemeiner Interessen Struktur-Test", status: 1, item: 1 },
+    { name: "Positive - Negative", status: 0, item: 2 },
+    { name: "Motivation", status: 0, item: 3 }
 ]
 
-export default function Main({model}) {
+export default function Main({ model }) {
     const router = useRouter()
-    const {user, isAuthenticated, saveUser, clearUser} = useAuth()
+    const { user, isAuthenticated, saveUser, clearUser } = useAuth()
 
     const [request, setRequest] = useState([])
 
@@ -108,11 +108,11 @@ export default function Main({model}) {
             if (existingResponseIndex > -1) {
                 // If found, update the existing response
                 return prev.map((response, index) =>
-                    index === existingResponseIndex ? {...response, value: newValue} : response
+                    index === existingResponseIndex ? { ...response, value: newValue } : response
                 )
             } else {
                 // If not found, add a new response
-                return [...prev, {question_id: question_id, value: newValue}]
+                return [...prev, { question_id: question_id, value: newValue }]
             }
         })
     }
@@ -140,25 +140,55 @@ export default function Main({model}) {
             <div className='input_mainbody'>
                 <main className='flex-row justify-between px-10 pt-10'>
                     <form onSubmit={handleFormSubmission}>
-                        <h1 className='tracking-wider text-xl'>
-                            {"Allgemeiner Interessen Struktur-Test"}
+                        <h1 className='tracking-wider text-xl mb-5'>
+                            {"Allgemeiner Interessen Struktur-Test (AIST)"}
                         </h1>
-                        <p className='mb-10'>
-                            This section covers questions about the AIST questionnaire.
+                        <p className='mb-10 text-lightgrey'>
+                            Der Allgemeine Interessen-Struktur-Test / Umwelt-Struktur-Test basiert auf der Berufswahl- und Laufbahntheorie des amerikanischen Psychologen John L. Holland. Diese Theorie geht davon aus, dass Persönlichkeitsmerkmale und Berufscharakteristika miteinander in Beziehung stehen. Holland meint, dass die Persönlichkeit eines Menschen sich in der Wahl der beruflichen Laufbahn widerspiegelt. Sein Ansatz lässt sich so zusammenfassen:
+
+                            <ul className="list-disc ml-10 mt-5 text-lightgrey">
+                                <li className="pb-2">Die Wahl eines Berufes ist Ausdruck der Persönlichkeit, obwohl auch günstige Gelegenheiten eine Rolle spielen.</li>
+                                <li className="pb-2">Menschen in derselben Berufsgruppe haben ähnliche Persönlichkeitsmerkmale und Laufbahnmuster.</li>
+                                <li className="pb-2">Menschen in derselben Berufsgruppe reagieren in bestimmten Berufssituationen ähnlich.</li>
+                                <li className="pb-2">Berufliche Leistung, Stabilität in der Laufbahn und Zufriedenheit im Beruf hängen davon ab, wie gut Persönlichkeitsmerkmale und berufliches Umfeld übereinstimmen.</li>
+                            </ul>
                         </p>
+                        <div className='flex items-center w-full my-2'>
+                            <div className='flex-grow'>
+                                <p className='text-lunapurple'>
+                                    Ich stimme den folgenden Aussagen zu: Ich mag es...
+                                </p>
+                            </div>
+                            <div className='flex justify-between w-72 text-lunapurple pr-1'>
+                                <p className='flex-1 grid py-1 justify-items-center rounded'>
+                                    Nicht gerne
+                                </p>
+                                <p className='flex-1 grid py-1 justify-items-center rounded'></p>
+                                <p className='flex-1 grid py-1 justify-items-center rounded'></p>
+                                <p className='flex-1 grid py-1 justify-items-center rounded'></p>
+                                <p className='flex-1 grid py-1 justify-items-center rounded'>
+                                    Gerne
+                                </p>
+                            </div>
+                        </div>
                         {AIST_questions.map((question, index) => {
                             return (
-                                <InputRow
-                                    key={question.id}
-                                    type='likert'
-                                    item={question}
-                                    options={[0,1,2,3,4,5]}
-                                    maintext={question.question}
-                                    subtitle={null}
-                                    onChange={(e) =>
-                                        handleResponseChange(question.id, e.target.value)
-                                    }
-                                />
+                                <>
+                                    <InputRow
+                                        key={question.id}
+                                        type='likert'
+                                        item={question}
+                                        options={[0, 1, 2, 3, 4, 5]}
+                                        maintext={question.question}
+                                        subtitle={null}
+                                        onChange={(e) =>
+                                            handleResponseChange(question.id, e.target.value)}
+
+
+                                    />
+                                    <div className={`block ${(index + 1) % 5 === 0 ? 'pb-5' : 'pb-0'}`} />
+
+                                </>
                             )
                         })}
 
