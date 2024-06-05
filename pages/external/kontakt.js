99ea4034fe204adbd44d59b30feee603b2fcbadf
Dennis Perrett <perrett.dennis@gmail.com>
2024-06-05 14:52:14 +0200

    readded external pages, fixed issue with image

---
+++ pages/external/kontakt.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/LandingNavBar"
+
+
+export default function Kontakt({ model }) {
+    return (
+        <div className='w-screen min-h-screen bg-pagebg'>
+            <div className='px-48 py-12'>
+                <Navbar theme={"dark"} />
+                <h1 className="text-center mt-48">Kommt bald</h1>
+            </div>
+        </div>
+    )
+}
