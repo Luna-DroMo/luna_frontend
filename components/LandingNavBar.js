36fdc93ac8ce2860a010ec868a6e15017d40789f
Dennis Perrett <perrett.dennis@gmail.com>
2024-06-05 13:49:59 +0200

    created empty external kontakt/about us/faq pages

--- components/LandingNavBar.js
+++ components/LandingNavBar.js
@@ -1,8 +1,10 @@
-export default function Navbar({theme}) {
-    let logo_source = "Logo.png"
+import Image from 'next/image';
+
+export default function Navbar({ theme }) {
+    let logo_source = `/Logo.png`
     let text_color = "white"
     if (theme === "dark") {
-        logo_source = "Logo.png"
+        logo_source = `/Logo.png`
         text_color = "lunapurple"
     }
 
@@ -10,20 +12,20 @@ export default function Navbar({theme}) {
         <div id='nav-links' className={`text-${text_color} text-large w-full`}>
             <ul className='space-x-6 flex flex-row justify-end items-center'>
                 <li className='hover:underline'>
-                    <a href='#'>Über Uns</a>
+                    <a href='/external/about_us'>Über Uns</a>
                 </li>
                 <li className='hover:underline'>
-                    <a href='#'>FAQs</a>
+                    <a href='/external/FAQ'>FAQs</a>
                 </li>
                 <li className='hover:underline'>
-                    <a href='#'>Kontakt</a>
+                    <a href='/external/kontakt'>Kontakt</a>
                 </li>
-                <li className='hover:underline'>
+                {/*<li className='hover:underline'>
                     <a href='#'>Datenschutz</a>
-                </li>
+                </li>*/}
                 <li className='pl-10'>
-                    <a href='/cockpit' className='flex items-center'>
-                        <img src={logo_source} className='h-10 inline-block' alt='logo' />
+                    <a href='/' className='flex items-center'>
+                        <Image src={logo_source} width={150} height={40} className='h-10 mr-5 my-3 inline-block' alt='logo' />
                     </a>
                 </li>
             </ul>
